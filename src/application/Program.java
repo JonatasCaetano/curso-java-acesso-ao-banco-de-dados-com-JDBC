c7d2b5d44799f0643668a6e94f7b469b5af510af
Jonatas Caetano <jonatas.calves@gmail.com>
2021-09-11 12:13:33 -0300

    Implementando insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 
+
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -24,11 +26,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n---teste 2: seller findbAll---");
+		System.out.println("\n---teste 3: seller findbAll---");
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
